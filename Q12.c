a0502086a123e77e2f2b86d0d3b3528467f00d8a
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 20:06:02 +0530

    Create Q12.c

---
+++ Q12.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    { int k='A';
+        for(j=1;j<=(2*n)-1;j++)
+        {
+            if(j>=i&&j<=(2*n)-i)
+            {
+                printf("%c",k);
+                j<n?k++:k--;
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
