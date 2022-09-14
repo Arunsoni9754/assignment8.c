4653b51d549d82dc7586bf47ebdfc9b6c902cb52
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 20:04:25 +0530

    Create Q10.c

---
+++ Q10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {int k=1;
+        for(j=1;j<=(2*n)-1;j++)
+        {
+            if(j<=n-i+1||j>=n+i-1)
+            {
+                printf("%d",k); 
+            j<n?k++:k--;
+            }
+            else
+            {
+                printf(" ");
+              if(j==(i+3)&&i>1)
+                k--;
+            }
+        }
+        printf("\n");
+    }
+}
